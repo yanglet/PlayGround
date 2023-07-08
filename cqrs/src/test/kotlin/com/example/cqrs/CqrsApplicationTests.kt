94a05378fa0d39b16502f2424cec45260a4c7439
yanglet <didcnddnr@skuniv.ac.kr>
2023-07-08 20:07:10 +0900

    add docker-compose

--- cqrs/src/test/kotlin/com/example/cqrs/CqrsApplicationTests.kt
+++ cqrs/src/test/kotlin/com/example/cqrs/CqrsApplicationTests.kt
@@ -3,11 +3,11 @@ package com.example.cqrs
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class CqrsApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class CqrsApplicationTests {
+//
+//	@Test
+//	fun contextLoads() {
+//	}
+//
+//}
