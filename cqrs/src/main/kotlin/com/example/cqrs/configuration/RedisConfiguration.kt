2daa5014ff376ea298315f4aaf7a22814ebf4f2e
yanglet <y2yone023@naver.com>
2023-07-10 10:04:01 +0900

    add redis config

---
+++ cqrs/src/main/kotlin/com/example/cqrs/configuration/RedisConfiguration.kt
@@ -0,0 +1,38 @@
+package com.example.cqrs.configuration
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+
+@Configuration
+class RedisConfiguration {
+    @Value("\${spring.data.redis.host}")
+    lateinit var host: String
+
+    @Value("\${spring.data.redis.port}")
+    lateinit var port: String
+
+    @Bean
+    fun redisConnectionFactory(): LettuceConnectionFactory =
+        LettuceConnectionFactory(host, port.toInt())
+
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        val template = RedisTemplate<String, Any>()
+        template.connectionFactory = redisConnectionFactory
+        template.keySerializer = StringRedisSerializer()
+        return template
+    }
+    @Bean
+    fun springSessionDefaultRedisSerializer(): RedisSerializer<Any?>? {
+        return GenericJackson2JsonRedisSerializer()
+    }
+}
