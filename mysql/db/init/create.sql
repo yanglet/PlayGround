6d16b25c9cef5887b82fc37e8e5b0ec3968cd3fe
yanglet <didcnddnr@skuniv.ac.kr>
2023-07-25 21:50:21 +0900

    DDL pk AI 추가

--- mysql/db/init/create.sql
+++ mysql/db/init/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `yanglet`.`MEMBER`
 (
-    `member_no`       INT UNSIGNED NOT NULL COMMENT '회원 일련번호',
+    `member_no`       INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '회원 일련번호',
     `name`            VARCHAR(50) NOT NULL COMMENT '회원 이름',
     `insert_date`     DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP (3) COMMENT '등록일자',
     `insert_operator` VARCHAR(50) NOT NULL COMMENT '등록자',
